6d165fd0f785f754ef9faca0e849199a0648afd9
Ajay Sarkate <115990815+ajay-s-07@users.noreply.github.com>
2023-03-18 15:50:33 +0530

    Update promiseToUpdateHtmlElement.js

---
+++ promiseToUpdateHtmlElement.js
@@ -0,0 +1,8 @@
+function func(){
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve("Hello, world!");
+		}, 1000);
+	});
+}
+func().then((value) => document.getElementById("output").innerText = value);
